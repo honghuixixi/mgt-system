ccadfb7c1a13659414e9a382b66aafd78495ddd4
李思渊 <334224109@qq.com>
2016-07-08 14:31:23 +0800

    个人信息

---
+++ mgt-facade-system/src/main/java/cn/qpwa/mgt/facade/system/service/RSAService.java
@@ -0,0 +1,42 @@
+package cn.qpwa.mgt.facade.system.service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * Service - RSA安全
+ * 
+ * @author 
+ * @version 1.0
+ */
+public interface RSAService {
+
+	/**
+	 * 生成密钥(添加私钥至Session并返回公钥)
+	 * 
+	 * @param request
+	 *            httpServletRequest
+	 * @return 公钥
+	 */
+	RSAPublicKey generateKey(HttpServletRequest request);
+
+	/**
+	 * 移除私钥
+	 * 
+	 * @param request
+	 *            httpServletRequest
+	 */
+	void removePrivateKey(HttpServletRequest request);
+
+	/**
+	 * 解密参数
+	 * 
+	 * @param name
+	 *            参数名称
+	 * @param request
+	 *            httpServletRequest
+	 * @return 解密内容
+	 */
+	String decryptParameter(String name, HttpServletRequest request);
+
+}
